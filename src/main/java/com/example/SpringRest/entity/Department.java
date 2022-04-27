9fc20b0b1cc78be4406d8a0bd850c7efca275dfa
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-27 11:36:02 +0300

    Created a simple pojo class

--- src/main/java/com/example/SpringRest/entity/Department.java
+++ src/main/java/com/example/SpringRest/entity/Department.java
@@ -17,4 +17,10 @@ import lombok.NoArgsConstructor;
 
 
 public class Department {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
 }
