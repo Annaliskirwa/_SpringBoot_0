5593c768d9b54e315fdfd9eb6004f931249b8daa
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-27 11:33:41 +0300

    Importing required packages and modules

---
+++ src/main/java/com/example/SpringRest/controller/DepartmentController.java
@@ -0,0 +1,4 @@
+package com.example.SpringRest.controller;
+
+public class DepartmentController {
+}
---
+++ src/main/java/com/example/SpringRest/entity/Department.java
@@ -0,0 +1,20 @@
+package com.example.SpringRest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+
+public class Department {
+}
