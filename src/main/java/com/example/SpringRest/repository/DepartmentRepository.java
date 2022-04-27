3491a77867886d23f887b6cd7f6c2facc475aa9b
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-27 11:44:47 +0300

    Created an inteface to extend the crud repo

---
+++ src/main/java/com/example/SpringRest/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.example.SpringRest.repository;
+
+import com.example.SpringRest.entity.Department;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends CrudRepository{
+}
