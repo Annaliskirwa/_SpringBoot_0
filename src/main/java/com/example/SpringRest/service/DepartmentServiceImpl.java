1e288d6b8341b88402166e24b59d83248f79b801
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-27 14:26:18 +0300

    import the required packages for the service

--- src/main/java/com/example/SpringRest/service/DepartmentServiceImpl.java
+++ src/main/java/com/example/SpringRest/service/DepartmentServiceImpl.java
@@ -1,4 +1,6 @@
 package com.example.SpringRest.service;
 
+import com.example.SpringRest.entity.Department;
+import com.example.SpringRest.repository.DepartmentRepository;
 public class DepartmentServiceImpl {
 }
