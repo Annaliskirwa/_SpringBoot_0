21a524783e23ab17dc6342b2037425d1fa199c0a
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-27 11:54:19 +0300

    CRUD operations

--- src/main/java/com/example/SpringRest/service/DepartmentService.java
+++ src/main/java/com/example/SpringRest/service/DepartmentService.java
@@ -5,5 +5,15 @@ import com.example.SpringRest.entity.Department;
 import java.util.List;
 
 public interface DepartmentService {
+//save operation
+    Department saveDepartment(Department department);
 
+//    Read operation
+    List<Department> fetchDepartmentList();
+
+//    Update operation
+    Department updateDepartment(Department department, Long departmentId);
+
+//    Delete operation
+    void deleteDepartmentById(Long departmentId);
 }
