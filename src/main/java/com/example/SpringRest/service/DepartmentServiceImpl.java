9d4ff4b20b13baf2fc780ea4de7e770003772dae
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-27 14:28:00 +0300

    imported the required classes for the service

--- src/main/java/com/example/SpringRest/service/DepartmentServiceImpl.java
+++ src/main/java/com/example/SpringRest/service/DepartmentServiceImpl.java
@@ -2,5 +2,10 @@ package com.example.SpringRest.service;
 
 import com.example.SpringRest.entity.Department;
 import com.example.SpringRest.repository.DepartmentRepository;
+
+import java.util.List;
+import java.util.Objects;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 public class DepartmentServiceImpl {
 }
