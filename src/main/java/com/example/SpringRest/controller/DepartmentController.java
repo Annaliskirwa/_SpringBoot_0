1825292d5a3922ebaba779b35cda3ad4f7925f83
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-27 15:08:06 +0300

    importing the required classes and packages for the controllers

--- src/main/java/com/example/SpringRest/controller/DepartmentController.java
+++ src/main/java/com/example/SpringRest/controller/DepartmentController.java
@@ -1,4 +1,13 @@
 package com.example.SpringRest.controller;
 
+import com.example.SpringRest.entity.Department;
+import com.example.SpringRest.service.DepartmentService;
+
+import java.util.List;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 public class DepartmentController {
 }
