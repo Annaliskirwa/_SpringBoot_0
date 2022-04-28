e7b91ed2be09c98fae9531b9e57b8d8777eee164
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-28 12:15:59 +0300

    generated getters and setters for the methods

--- src/main/java/com/example/PojoBean/Employee.java
+++ src/main/java/com/example/PojoBean/Employee.java
@@ -18,4 +18,32 @@ public class Employee {
         this.id = id;
         this.salary = salary;
     }
-}
+
+    //getter and setter methods
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public double getSalary() {
+            return salary;
+        }
+
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+
+
+    }
